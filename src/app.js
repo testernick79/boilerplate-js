04f051beb04ed65eee9e3ffcbe1f6b4024495be9
nick <123@gmail.com>
2020-04-15 15:23:38 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import User from './components/User';
+
+import './scss/app.scss';
+import './scss/card.scss';
+
+const app = async () => {
+    document.getElementById( 'header' ).innerHTML = Header();
+    document.getElementById( 'user' ).innerHTML = await User();
+}
+
+app();
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    const template = `
+        <header>
+            <h1>My Parcel App</h1>
+            <p>Boiler plate JS app</p>
+        </header>
+    `;
+
+    return template;
+
+}
+export default Header;
